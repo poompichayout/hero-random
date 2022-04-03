90db5afe2ea7eb152ae487909049e5c35763d2bf
poompichayout <poompichayout.kon@dome.tu.ac.th>
2022-04-03 22:25:33 +0700

    ADD: footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import React from "react";
+import { Detail, FooterWrapper, LeftColumn, RightColumn } from "./style";
 
 const Footer = () => {
-  return (
-	<div>Footer</div>
-  )
-}
+	return (
+		<FooterWrapper>
+			<LeftColumn>
+				<h2>TRADE</h2>
+				<p>2010-2020</p>
+				<p>Privacy - Terms</p>
+			</LeftColumn>
+			<RightColumn>
+				<Detail>
+					<h4>Products</h4>
+					<p>Product</p>
+					<p>Product</p>
+					<p>Product</p>
+				</Detail>
+				<Detail>
+					<h4>Features</h4>
+					<p>Feature</p>
+					<p>Feature</p>
+					<p>Feature</p>
+				</Detail>
+				<Detail>
+					<h4>Resources</h4>
+					<p>Resource</p>
+					<p>Resource</p>
+					<p>Resource</p>
+				</Detail>
+				<Detail>
+					<h4>Company</h4>
+					<p>Company</p>
+					<p>Company</p>
+					<p>Company</p>
+				</Detail>
+			</RightColumn>
+		</FooterWrapper>
+	);
+};
 
-export default Footer
+export default Footer;
---
+++ src/components/Footer/style.jsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+export const FooterWrapper = styled.div`
+	display: flex;
+	padding: 50px 100px;
+	color: white;
+	background-color: #6390D4;
+
+	p {
+		font-weight: 100;
+	}
+`;
+
+export const LeftColumn = styled.div`
+	
+`;
+
+export const RightColumn = styled.div`
+	margin-left: auto;
+	display: flex;
+`;
+
+export const Detail = styled.div`
+	margin: 0 5em;
+`;
